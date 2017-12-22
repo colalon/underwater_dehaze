b48f0ffdbf120a158229d8bd71f3b7f5bfc854dc
colalon <bat8366@gmail.com>
2017-12-23 00:12:55 +0800

    v2

--- demo.py
+++ demo.py
@@ -6,12 +6,12 @@ import time
 
 start = time.time()
 
-im = cv2.imread('demo.png')/255.0
+im = cv2.imread('demo.jpg')/255.0
 
 result = dehaze.dehaze(im,1,15)
 
 result = np.uint8(result)
 
-cv2.imwrite('result.png',result)
+cv2.imwrite('result.jpg',result)
 
 print ('time:',time.time()-start)
